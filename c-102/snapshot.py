055e5ba5d914db62e4d9175b144c2fed6d1bc167
samadrita2145 <86518116+samadrita2145@users.noreply.github.com>
2022-08-03 18:38:13 +0530

    Add files via upload

---
+++ c-102/snapshot.py
@@ -0,0 +1,11 @@
+import cv2
+def takeSnapshot():
+    videoCaptureObject=cv2.VideoCapture(0)
+    result=True
+    while(result):
+        ret,frame=videoCaptureObject.read()
+        cv2.imwrite("Picature1.jpg",frame)
+        result=False
+    videoCaptureObject.release()
+    cv2.destroyAllWindows()
+takeSnapshot()
